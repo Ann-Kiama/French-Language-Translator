579a4a4b342c2829423d8ec8104632acc7653a6e
Ann-Kiama <annkiama277@gmail.com>
2026-07-10 12:13:30 +0300

    Added the AI API Intergration

--- src/script.js
+++ src/script.js
@@ -1,12 +1,30 @@
-function generatePoem(event){
-    event.preventDefault();
-
-    new Typewriter("#poem", {
-  strings:"Demain, dès l’aube, à l’heure où blanchit la campagne,",
+function displayPoem(response){
+   new Typewriter("#poem", {
+  strings:response.data.answer ,
   autoStart: true,
   delay:1,
   cursor:"",
 });
+    
+}
+
+function generatePoem(event){
+    event.preventDefault();
+
+let instructionsInput=document.querySelector("#user-instructions");
+let apiKey="281d3c04932a5df0f10bbof67859adta";
+let prompt=`User instructions: Generate a French poem about ${instructionsInput.value}`;
+let context=
+"You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem  and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+
+
+axios.get(apiUrl).then(displayPoem);
+
+
+   
 }
 
 
